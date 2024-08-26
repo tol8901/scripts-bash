ad6d7aebe51e06f77a8dac22083f35e6e4fa6a45
Anatolii Kravchenko <39213432+tol8901@users.noreply.github.com>
2024-08-26 12:24:41 +0200

    Added regex check of the zip-code

--- zip_code_checker.sh
+++ zip_code_checker.sh
@@ -3,7 +3,9 @@
 # Check if the user has provided a zip code
 # (we are using standard United States zip codes, a 5 digit number)
 read -p "Enter your ZIP code: " zip
-if [ $zip -eq 9999999 ]
+if echo $zip | grep -E '^[0-9]+$' > /dev/null 2>/dev/null
 then
-    echo "You are in the Mars zip code"
+    echo "Thank you for the proper zip code"
+else
+    echo "Incorrect zip code"
 fi
